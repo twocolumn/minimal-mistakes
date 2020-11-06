ba0b76609ec380b6788fcfda9bbc37257138d180
Kamil Adam <kamil.adam.zabinski@gmail.com>
2020-11-06 18:36:50 +0100

    Update assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,17 @@
 ---
 layout: compress
 ---
-{% include js/target-blank.js %}
-
-{% include js/cookie-consent.js %}
+{% comment %}
+{% if site.google.search_instant %}
+  {% include js/google_search_instant.js %}
+{% endif %}
+{% endcomment %}
 
 {% case site.comments.provider %}
   {% when "disqus_loader" %}
-    {%- include js/disqus-loader.js -%}
-  {% when "disqus_empty" %}
-    {%- include js/disqus-empty.js -%}
+    {%- include js/disqus-click.js -%}
+  {% when "disqus_scroll" %}
+    {%- include js/disqus-scroll.js -%}
 {% endcase %}
-
-{% include js/google_search_instant.js %}
-
-{% include js/google_search_instant.js %}
 
 console.log("Loaded app.js");
---
+++ assets/js/scripts/cookie-consent.js
@@ -0,0 +1,59 @@
+$(function() {
+  function createExpires(days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    return "; expires=" + date.toUTCString();
+  }
+  function createCookie(name,value,days) {
+    const expires = days ? createExpires(days) : "";
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+  function readCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(var i=0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0)==' ') {
+        c = c.substring(1, c.length);
+      }
+      if (c.indexOf(nameEQ) == 0) {
+        return c.substring(nameEQ.length,c.length);
+      }
+    }
+    return null;
+  }
+  function eraseCookie(name) {
+    createCookie(name,"",-1);
+  }
+  function isTrue(value) {
+    return value == 'true'
+  }
+
+  function loadAdditionalScript() {
+    if (typeof INITIATE === 'undefined') {
+      INITIATE = null;
+      loadScript('/assets/js/secondary.min.js');
+    }
+  }
+
+  const cookieNoticeDismissedKey = 'cookie-notice-dismissed';
+  const cookieNoticeDismissed = isTrue(readCookie(cookieNoticeDismissedKey));
+  const $cookieNotice = $('#cookie-notice');
+  if(!cookieNoticeDismissed) {
+    $cookieNotice.show();
+  } else {
+    loadAdditionalScript()
+  }
+  $('#cookie-notice-show').click(function() {
+    $cookieNotice.show();
+    loadAdditionalScript()
+  });
+  $('#cookie-notice-accept').click(function() {
+    createCookie(cookieNoticeDismissedKey,'true',31);
+    $cookieNotice.hide();
+  });
+  $("body").one( "click mouseover scroll", function(event) {
+    loadAdditionalScript()
+  });
+  console.log("Load primary")
+});
