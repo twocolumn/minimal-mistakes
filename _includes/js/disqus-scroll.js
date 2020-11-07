c5790b723ee275318a1825dcb19b0c085fbdffcf
Kamil Adam <kamil.adam.zabinski@gmail.com>
2020-11-07 19:35:41 +0100

    Update _includes

--- _includes/js/disqus-scroll.js
+++ _includes/js/disqus-scroll.js
@@ -6,9 +6,9 @@ $(document).scroll(function(e) {
     const $disqus_thread = $('#disqus_thread');
 
     if ($disqus_thread[0].getBoundingClientRect().top - 150 < $window.scrollTop()) {
-      const po = document.createElement('script');
-      po.src = '//{{site.comments.disqus.shortname}}.disqus.com/embed.js';
-      const s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+      const s = document.createElement('script');
+      s.src = '//{{site.comments.disqus.shortname}}.disqus.com/embed.js';
+      $('script')[0].before(s);
       $disqus_empty.remove();
       console.log('Load discus.');
     }
