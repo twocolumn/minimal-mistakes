7a3aabdd6f6dfbb103fff7eb4f270046b25b2fe4
Kamil Adam <kamil.adam.zabinski@gmail.com>
2020-11-07 22:57:48 +0100

    Update _includes

--- _includes/js/fa-scroll.js
+++ _includes/js/fa-scroll.js
@@ -1,10 +1,7 @@
 $(document).scroll(function(e) {
 
   if (typeof FA === 'undefined') {
-    const $window = $(window);
-    const $page__share = $('.page__share')[0];
-
-    if ($page__share[0].getBoundingClientRect().top - 150 < $window.scrollTop()) {
+    if ($('.page__share')[0].getBoundingClientRect().top - 150 < $(window).scrollTop()) {
       const s = document.createElement('script');
       s.src = '/assets/js/fontawesome/all.slim.min.js';
       $('script')[0].before(s);
