d51467905cf442a01f396bd50ef71346553f9415
Kamil Adam <kamil.adam.zabinski@gmail.com>
2020-11-07 22:33:28 +0100

    Update _includes

---
+++ _includes/js/fa-scroll.js
@@ -0,0 +1,15 @@
+$(document).scroll(function(e) {
+
+  if (FA === undefined) {
+    const $window = $(window);
+    const $page__share = $('.page__share')[0];
+
+    if ($page__share[0].getBoundingClientRect().top - 150 < $window.scrollTop()) {
+      const s = document.createElement('script');
+      s.src = '/assets/js/fontawesome/all.slim.min.js';
+      $('script')[0].before(s);
+      $disqus_empty.remove();
+      console.log('Load fa.');
+    }
+  }
+});
