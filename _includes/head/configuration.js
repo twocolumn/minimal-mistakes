f1cf8e48218b5882044b38a97a5848db890c2cc9
Kamil Adam <kamil.adam.zabinski@gmail.com>
2020-11-11 18:14:04 +0100

    Update _includes

--- _includes/head/configuration.js
+++ _includes/head/configuration.js
@@ -4,6 +4,7 @@ CONF = {
   comments_provider:"{{site.comments.provider}}",
   comments_disqus_shortname:"{{site.comments.disqus.shortname}}",
   search:"{{site.search}}",
+  search_fallow:"{{site.search_follow}}",
   search_provider:"{{site.search_provider}}",
   google_search_engine_id:"{{site.google.search_engine_id}}",
   google_search_instant:"{{site.google.search_instant}}"
