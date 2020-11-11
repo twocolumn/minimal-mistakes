83e4919d6f2eb078ba306fb54945a6e8647ad6c9
Kamil Adam <kamil.adam.zabinski@gmail.com>
2020-11-11 11:30:54 +0100

    Update _includes

--- _includes/head/configuration.js
+++ _includes/head/configuration.js
@@ -1,9 +1,6 @@
----
----
 CONF = {
   baseurl:{{site.baseurl}},
   comments_provider:{{site.comments.provider}},
   comments:{{site.comments.provider and page.comments}},
   google_search_instant:{{site.google.search_instant}}
 };
-
