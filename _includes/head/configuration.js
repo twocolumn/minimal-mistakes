d865b5d3ab9725acd9d4e3f8535d89d0d3d5103e
Kamil Adam <kamil.adam.zabinski@gmail.com>
2021-03-15 14:20:26 +0100

    Update _includes

---
+++ _includes/head/configuration.js
@@ -0,0 +1,11 @@
+CONF = {
+  baseurl:"{{site.baseurl}}",
+  comments:"{{page.comments}}",
+  comments_provider:"{{site.comments.provider}}",
+  comments_disqus_shortname:"{{site.comments.disqus.shortname}}",
+  search:"{{site.search}}",
+  search_fallow:"{{site.search_follow}}",
+  search_provider:"{{site.search_provider}}",
+  google_search_engine_id:"{{site.google.search_engine_id}}",
+  google_search_instant:"{{site.google.search_instant}}"
+};
